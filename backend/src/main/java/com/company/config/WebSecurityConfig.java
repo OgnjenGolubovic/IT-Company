48a6c4a933df1cececa6b0ecbbebb8e10f43d475
TheWhiteRabbit21 <milutinovicn21@gmail.com>
2023-05-24 23:07:22 +0200

    Update WebSecurityConfig.java

--- backend/src/main/java/com/company/config/WebSecurityConfig.java
+++ backend/src/main/java/com/company/config/WebSecurityConfig.java
@@ -120,8 +120,6 @@ public class WebSecurityConfig {
 
 				.antMatchers(HttpMethod.POST, "/auth/login")
 
-				.antMatchers(HttpMethod.GET, "/api/admin/regRequestAll")
-
     			// Ovim smo dozvolili pristup statickim resursima aplikacije
     			.antMatchers(HttpMethod.GET, "/", "/webjars/**", "/*.html", "favicon.ico",
     			"/**/*.html", "/**/*.css", "/**/*.js");
